bd8bf67aacf8c41df8d85737c89658d1ff598825
tristan <thuckabee.work@gmail.com>
2023-11-02 17:48:11 -0500

    done with 01

--- 01-serve-static-assets/server.js
+++ 01-serve-static-assets/server.js
@@ -2,7 +2,23 @@ const http = require('http');
 const fs = require("fs");
 
 const server = http.createServer((req, res) => {
-  // Your code here
+  const html = fs.readFileSync('./index.html');
+
+  if (req.url.startsWith('/static')) {
+    let filepath = req.url.split('static')[1];
+    console.log(`./assets${filepath}`)
+    let file = fs.readFileSync(`./assets${filepath}`);
+
+    
+    res.statusCode = 200;
+    if (filepath.startsWith('/images')) res.setHeader('Content-Type', 'images/jpeg');
+    if (filepath.startsWith('/css')) res.setHeader('Content-Type', 'text/css');
+    return res.end(file);
+  }
+
+  res.statusCode = 200;
+  res.setHeader('Content-Type', 'text/html');
+  return res.end(html);
 });
 
 const port = 5000;
